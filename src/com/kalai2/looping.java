c1f4ef405f680e044a0173e62705c5da315d1cef
kalaiarasan2909 <kalaiarasans2909@gmail.com>
2024-01-19 11:50:18 +0530

    initialised of git

---
+++ src/com/kalai2/looping.java
@@ -0,0 +1,24 @@
+package com.kalai2;
+
+public class looping {
+	
+	public static void main(String[] args) {
+	
+
+	int b=10;
+	
+	do {
+   System.out.println(b);
+   b++;
+   
+	}
+	while(b<15);
+}
+
+}
+	
+	
+	
+
+
+
